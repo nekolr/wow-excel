b8b47fb0991822ce9235d924751c01dabc3f8b0a
nekolr <excalibll@163.com>
2020-05-09 11:51:25 +0800

    Update comment

--- wow-excel-esr/src/main/java/com/nekolr/read/processor/ExcelStreamReadProcessor.java
+++ wow-excel-esr/src/main/java/com/nekolr/read/processor/ExcelStreamReadProcessor.java
@@ -11,11 +11,11 @@ import org.apache.poi.ss.usermodel.Workbook;
  * <p>
  * 使用 Excel Streaming Reader 读取 xlsx 类型的文档
  *
- * @param <R>
+ * @param <R> 使用 @Excel 注解的类类型
  */
 public class ExcelStreamReadProcessor<R> extends DefaultExcelReadProcessor<R> {
     @Override
-    public void init(ExcelReadContext readContext) {
+    public void init(ExcelReadContext<R> readContext) {
         Excel excel = readContext.getExcel();
         if (readContext.isStreamingReaderEnabled()) {
             if (excel.getExcelType().equals(ExcelType.XLSX)) {
